1d81256e9b108f8cd244f11a6e28b14c09250090
Tshimega Molefe <tshimegabelmont@gmail.com>
2023-08-11 18:00:17 +0200

    Task3: Completed Compulstory Task1

---
+++ Task3/hello_world.py
@@ -0,0 +1,20 @@
+#  =========  The Input Output Task =========
+# Request for the user to enter their name
+# Create an input command to create and store the users name
+# Initialize a variable to store the users name
+# Output/Print the name variable
+
+# Request for the user to enter their age
+# Create an input command to create and store the users age
+# Initialize a variable to store the users age
+# Output/Print the age variable
+
+# Output/Print the string `hello world`
+
+name = input("Enter your name: ")
+print(name)
+
+age = input("Enter your age: ")
+print(age)
+
+print("Hello World")
