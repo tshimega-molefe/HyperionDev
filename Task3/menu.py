4fe2e36c3cf9088ae600abfda7663b12cb6625b0
Tshimega Molefe <tshimegabelmont@gmail.com>
2023-08-11 23:42:40 +0200

    Completed Task 3

---
+++ Task3/menu.py
@@ -0,0 +1,31 @@
+# Request for the user to enter their three favourite food items on a menu
+
+print("Please enter your 3 favourite food items on a menu")
+
+    # Request the user to enter item 1
+        # Initialize item1 = input(Fav item 1)
+        
+item1 = input("Enter item 1: ")
+    
+    # Request the user to enter item 2
+        # Initialize item2 = input(Fav item 2)
+        
+item2 = input("Enter item 2: ")
+    
+    # Request the user to enter item 3
+        # Initialize item3 = input(Fav item 3)
+        
+item3 = input("Enter item 3: ")
+        
+    
+# Output to the user their favourite items on a new line, with the opening message: "Order confirmation, you have ordered."
+
+print("Order confirmation, your have ordered:")
+print(item1)
+print(item2)
+print(item3)
+
+# print("Order confirmation, your have ordered: {}, {}, and {}".format(item1, item2, item3))
+
+
+
