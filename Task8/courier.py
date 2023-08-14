db23b99d518cfaec6fdfb076dac42fa9f828c723
Tshimega Molefe <tshimegabelmont@gmail.com>
2023-08-14 12:29:26 +0200

    Initialized Task8

---
+++ Task8/courier.py
@@ -0,0 +1,15 @@
+# We're trying to calculate the cost of sending a parcel for a courier company.
+# The variables to consider are:
+# Distance
+# Freight
+# Insurance
+# Gift
+# Priority
+# Parcel
+
+# Create an algorithm using these variables to find the total cost, stored as
+# total_cost = freight + insurance + gift + priority + parcel
+
+# Request the distance
+
+distance = str(input("Enter the total distance of the delivery in kilometers: "))
