da0504e6284669fd39d54167358f7f32166be5b9
Tshimega Molefe <tshimegabelmont@gmail.com>
2023-11-13 03:21:25 +0200

    Added header

--- Level2/Task19/hyperion/blog/models.py
+++ Level2/Task19/hyperion/blog/models.py
@@ -4,8 +4,12 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     # Create the primary keys, i.e. headings for each column in the database table
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=140)
     body = models.TextField()
+    signature = models.CharField(
+        max_length=140, default="What if man is not really a SCOUNDREL..."
+    )
     date = models.DateTimeField()
 
 
